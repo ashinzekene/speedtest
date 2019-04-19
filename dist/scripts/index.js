11be70daa05b25a918e37c483c4f8048aaf85ee7
Ekene Ashinze <ashinzekene@Ekenes-MacBook-Pro.local>
2019-04-19 12:17:52 +0100

    Add service worker

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -44,4 +44,16 @@
     setSpeedText(`${speed}mbps`)
     speedTypeElement.innerHTML = type
   }
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./dist/scripts/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 })()
---
+++ dist/scripts/sw.js
@@ -0,0 +1,20 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/speedtest/',
+  '/speedtest/manifest.json',
+  '/speedtest/dist/css/index.css',
+  '/speedtest/dist/scripts/index.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then((response => response || fetch(event.request))
+    )
+  );
+});
